dd78453f319da973904831ae3ced8333f12a0b77
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/CompanyManager.php
+++ src/Managers/CompanyManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Company newEntity()
+ * @method \Amethyst\Schemas\CompanySchema getSchema()
+ * @method \Amethyst\Repositories\CompanyRepository getRepository()
+ * @method \Amethyst\Serializers\CompanySerializer getSerializer()
+ * @method \Amethyst\Validators\CompanyValidator getValidator()
+ * @method \Amethyst\Authorizers\CompanyAuthorizer getAuthorizer()
+ */
 class CompanyManager extends Manager
 {
     use ConfigurableManager;
