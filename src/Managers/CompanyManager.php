d7a1500f027bffae5d99d545ae01a64ed8c3173a
railken <railken@protonmail.ch>
2019-07-14 13:07:13 +0200

    fix style

--- src/Managers/CompanyManager.php
+++ src/Managers/CompanyManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Company newEntity()
- * @method \Amethyst\Schemas\CompanySchema getSchema()
+ * @method \Amethyst\Models\Company                 newEntity()
+ * @method \Amethyst\Schemas\CompanySchema          getSchema()
  * @method \Amethyst\Repositories\CompanyRepository getRepository()
- * @method \Amethyst\Serializers\CompanySerializer getSerializer()
- * @method \Amethyst\Validators\CompanyValidator getValidator()
- * @method \Amethyst\Authorizers\CompanyAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\CompanySerializer  getSerializer()
+ * @method \Amethyst\Validators\CompanyValidator    getValidator()
+ * @method \Amethyst\Authorizers\CompanyAuthorizer  getAuthorizer()
  */
 class CompanyManager extends Manager
 {
